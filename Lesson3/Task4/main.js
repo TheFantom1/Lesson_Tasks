2bd9475e6fa0ab0b104bba726fa7551590130ca6
TheFantom1 <147336863+TheFantom1@users.noreply.github.com>
2023-11-27 23:43:01 +0200

    Add files via upload

---
+++ Lesson3/Task4/main.js
@@ -0,0 +1,56 @@
+// є масив
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+// за допомоги циклу вивести:
+//     - користувачів зі статусом true
+// - користувачів зі статусом false
+// - користувачів які старші за 30 років
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+
+document.write('<h2>Користувачі зі статусом true:</h2>');
+for (let user of users) {
+    if (user.status === true) {
+        document.write(`<p>${user.name}</p>`);
+    }
+}
+
+
+document.write('<h2>Користувачі зі статусом false:</h2>');
+for (let user of users) {
+    if (user.status === false) {
+        document.write(`<p>${user.name}</p>`);
+    }
+}
+
+
+document.write('<h2>Користувачі старші за 30 років:</h2>');
+for (let user of users) {
+    if (user.age > 30) {
+        document.write(`<p>${user.name}</p>`);
+    }
+}
